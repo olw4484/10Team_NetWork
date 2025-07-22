f77b59a2415749dad67a121656fdc5afdd63afe9
NormalSeed <dlrjsghk11@naver.com>
2025-07-22 17:30:55 +0900

    [Feat] Player Move
    마우스 우클릭 버튼을 눌러 플레이어의 목적지를 지정하는 기능 구현

--- Assets/00_WorkSpace/NormalSeed/Scripts/Hero/HeroController.cs
+++ Assets/00_WorkSpace/NormalSeed/Scripts/Hero/HeroController.cs
@@ -8,7 +8,14 @@ public class HeroController : MonoBehaviour, LGH_IDamagable
     protected HeroView view;
     protected HeroMovement mov;
 
+    private int heroType;
     private bool isInCombat;
+
+    public readonly int IDLE_HASH = Animator.StringToHash("Idle");
+    public readonly int MOVE_HASH = Animator.StringToHash("Move");
+    public readonly int ATTACK_HASH = Animator.StringToHash("Attack");
+    public readonly int DEAD_HASH = Animator.StringToHash("Dead");
+    // 각 Hero마다 스킬 애니메이션 존재
 
     private void Awake() => Init();
 
@@ -17,11 +24,20 @@ public class HeroController : MonoBehaviour, LGH_IDamagable
         model = GetComponent<HeroModel>();
         view = GetComponent<HeroView>();
         mov = GetComponent<HeroMovement>();
+
+        // 임시로 Hero1을 선택한 것으로 가정
+        heroType = 0;
+        model.GetInitStats(heroType);
     }
 
     private void FixedUpdate()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            mov.GetMoveDestination();
+        }
+
+        mov.Move(model.MoveSpd);
     }
 
     public void GetHeal(int amount)
--- Assets/00_WorkSpace/NormalSeed/Scripts/Hero/HeroMovement.cs
+++ Assets/00_WorkSpace/NormalSeed/Scripts/Hero/HeroMovement.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour
 {
-    
+    [SerializeField] Camera camera;
+
+    private bool isMove;
+    private Vector3 destination;
+
+    private void Awake() => Init();
+
+    private void Init()
+    {
+        camera = Camera.main;
+    }
+
+    public void GetMoveDestination()
+    {
+        // 카메라에서 빛을 쏴서 Ground에 맞았을 때 그곳을 목표지점으로 설정 
+        RaycastHit hit;
+        if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            SetDestination(hit.point);
+        }
+    }
+
+    public void SetDestination(Vector3 dest)
+    {
+        destination = dest;
+        isMove = true;
+    }
+
+    public void Move(float moveSpd)
+    {
+        if (isMove)
+        {
+            var dir = destination - transform.position;
+            // Hero의 position을 dir 방향으로 프레임과 상관없이 단위시간당 moveSpd만큼 이동
+            transform.position += dir.normalized * Time.deltaTime * moveSpd;
+        }
+
+        // 목적지에 충분히 가까워지면 이동 중지
+        if (Vector3.Distance(transform.position, destination) <= 0.1f)
+        {
+            isMove = false;
+        }
+    }
 }
