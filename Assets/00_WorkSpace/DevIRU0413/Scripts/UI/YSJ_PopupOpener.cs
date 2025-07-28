8f9a4e7bb48912064330fdc57fe0b0480e0f3472
DevIRU <deviru0413@gmail.com>
2025-07-28 15:02:45 +0900

    [Feat]: 팝업 오프너 기능 추가
    YSJ_PopupOpener

---
+++ Assets/00_WorkSpace/DevIRU0413/Scripts/UI/YSJ_PopupOpener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Runtime.UI
+{
+    public class YSJ_PopupOpener : MonoBehaviour
+    {
+        [Tooltip("Start 시 자동으로 팝업 열지 여부")]
+        public bool m_isStartOpenPopup = false;
+
+        [Tooltip("열릴 팝업 프리팹")]
+        public GameObject popupPrefab;
+
+        protected virtual void Start()
+        {
+            if (m_isStartOpenPopup)
+                OpenPopup();
+        }
+
+        public virtual void OpenPopup()
+        {
+            if (popupPrefab == null)
+            {
+                Debug.LogWarning("[PopupOpener] popupPrefab이 설정되지 않았습니다.");
+                return;
+            }
+
+            // 캔버스 매니저 또는 UIManager에서 부모 가져오기
+            var popupCanvas = YSJ_UIManager.Instance?.GetCanvas(YSJ_UIEnums.Popup);
+            if (popupCanvas == null)
+            {
+                Debug.LogError("[PopupOpener] PopupRoot가 UIManager에 설정되어 있지 않습니다.");
+                return;
+
+            }
+
+            var popup = YSJ_UISpawnFactory.ShowPopup(popupPrefab);
+        }
+    }
+}
