823ca6681c4195c1c4bc5b2ba32cef44e87761d9
coffeelatt <ssongcoffee@gmail.com>
2025-07-29 21:06:20 +0900

    [fix] ui추가작업,코드연결,짧은리펙토링

--- Assets/00_WorkSpace/coffeelatt/Scripts/SHI_InventorySlot.cs
+++ Assets/00_WorkSpace/coffeelatt/Scripts/SHI_InventorySlot.cs
@@ -1,21 +1,24 @@
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class SHI_InventorySlot : MonoBehaviour
 {
     public Image icon;
+    public Outline outline;
     public Button useButton;
     [Header("정상착용Debug")]
     [SerializeField] private SHI_ItemBase currentItem;
     private SHI_ItemManager inventoryManager;
 
+
+    
     private void Start()
     {
         inventoryManager = GameObject.Find("itemmanager").GetComponent<SHI_ItemManager>();
         useButton.onClick.AddListener(OnUseButtonClicked);
         currentItem = GetComponent<SHI_ItemBase>();
-        
+        useButton.interactable = inventoryManager.stat.CurHP.Value > 0;
     }
 
     public void SetItem(SHI_ItemBase item)
@@ -25,6 +28,7 @@ public class SHI_InventorySlot : MonoBehaviour
         //icon.sprite = item._Image;
         icon.enabled = true;
         useButton.interactable = true;
+
     }
 
     public void ClearSlot()
@@ -33,19 +37,26 @@ public class SHI_InventorySlot : MonoBehaviour
         icon.sprite = null;
         icon.enabled = false;
         useButton.interactable = false;
+        outline.enabled = false; // 슬롯의 아웃라인 비활성화
     }
 
     private void OnUseButtonClicked()
     {
         if (currentItem != null)
         {
-            
+
             bool success = inventoryManager.UseItem(currentItem);
+            if (currentItem.type == 2)
+                outline.enabled = true; // 슬롯의 아웃라인 활성화
+
+            else
+                outline.enabled = false; // 슬롯의 아웃라인 비활성화
+
             if (success)
-            { 
-            SHI_Inventory.instance.RemoveItem(currentItem);
-            if (currentItem.type <= 0)
-            ClearSlot();
+            {
+                SHI_Inventory.instance.RemoveItem(currentItem);
+                if (currentItem.type <= 0)
+                    ClearSlot();
             }
         }
     }
