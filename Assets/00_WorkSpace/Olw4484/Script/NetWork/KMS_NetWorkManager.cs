4ef69142f0bf859871ca1caa2dbe721e0f17484d
owl4484 <rodeo23267286@gmail.com>
2025-07-30 18:30:47 +0900

    [Feat] Create a NetworkManager DebugMode
    - 네트워크매니저 디버그 모드 제작

--- Assets/00_WorkSpace/Olw4484/Script/NetWork/KMS_NetWorkManager.cs
+++ Assets/00_WorkSpace/Olw4484/Script/NetWork/KMS_NetWorkManager.cs
@@ -3,6 +3,7 @@ using Photon.Realtime;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 
 //public enum TestTeamSetting
@@ -24,6 +25,11 @@ public class KMS_NetWorkManager : MonoBehaviourPunCallbacks
 
     public GameObject canvasPrefab;
 
+    // 디버깅 모드
+#if UNITY_EDITOR
+    public bool isHeroDebugMode = false;
+    public bool isCommandDebugMode = false;
+#endif
     private void Start()
     {
         string randomName = $"Tester{UnityEngine.Random.Range(1000, 9999)}";
@@ -52,7 +58,16 @@ public class KMS_NetWorkManager : MonoBehaviourPunCallbacks
     // 방에 들어왔을 때 팀/역할 배정 (MasterClient만 처리 - 동시 접속으로 인한 중복방지)
     public override void OnJoinedRoom()
     {
-        if (PhotonNetwork.IsMasterClient)
+        if (isCommandDebugMode)
+        {
+            PhotonNetwork.Instantiate("HQ", cmdRedSpawnPoint.position, cmdRedSpawnPoint.rotation);
+        }
+        if (isHeroDebugMode)
+        {
+            PhotonNetwork.Instantiate("Hero1", heroRedSpawnPoint.position, heroRedSpawnPoint.rotation);
+        }
+
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.PlayerList.Length == 4)
         {
             int red = 0, blue = 0;
             foreach (var player in PhotonNetwork.PlayerList)
@@ -100,19 +115,23 @@ public class KMS_NetWorkManager : MonoBehaviourPunCallbacks
 
         if (myRole == "Hero")
         {
-            // 팀에 따라 Hero 스폰
+            // Hero만 생성
             Vector3 pos = (myTeamId == 0) ? heroRedSpawnPoint.position : heroBlueSpawnPoint.position;
             Quaternion rot = (myTeamId == 0) ? heroRedSpawnPoint.rotation : heroBlueSpawnPoint.rotation;
             PhotonNetwork.Instantiate("Hero1", pos, rot);
         }
         else if (myRole == "Command")
         {
-            // 팀에 따라 CommandPlayer 스폰 및 Canvas 세팅
+            // 1. HQ 먼저 생성 (내가 그 팀의 Command라면)
+            Vector3 hqPos = (myTeamId == 0) ? hqRedSpawnPoint.position : hqBlueSpawnPoint.position;
+            Quaternion hqRot = (myTeamId == 0) ? hqRedSpawnPoint.rotation : hqBlueSpawnPoint.rotation;
+            PhotonNetwork.Instantiate("HQ", hqPos, hqRot);
+
+            // 2. CommandPlayer 생성 및 Canvas 연결
             Vector3 pos = (myTeamId == 0) ? cmdRedSpawnPoint.position : cmdBlueSpawnPoint.position;
             Quaternion rot = (myTeamId == 0) ? cmdRedSpawnPoint.rotation : cmdBlueSpawnPoint.rotation;
             var cmdObj = PhotonNetwork.Instantiate("CommandPlayer", pos, rot);
 
-            // 내 플레이어(로컬)만 Canvas 인스턴스 생성 및 연결
             if (cmdObj.GetComponent<PhotonView>().IsMine)
             {
                 var canvasObj = Instantiate(canvasPrefab);
@@ -123,4 +142,36 @@ public class KMS_NetWorkManager : MonoBehaviourPunCallbacks
             }
         }
     }
+    #region DebugMode
+    private void DebugHeroSpawn()
+    {
+        Vector3 pos = heroRedSpawnPoint.position;
+        Quaternion rot = heroRedSpawnPoint.rotation;
+
+        Instantiate(Resources.Load<GameObject>("Hero1"), pos, rot);
+    }
+
+    private void DebugHQspwn()
+    {
+        // HQ를 네트워크로 생성
+        Vector3 hqPos = hqRedSpawnPoint.position;
+        Quaternion hqRot = hqRedSpawnPoint.rotation;
+        PhotonNetwork.Instantiate("HQ", hqPos, hqRot);
+
+        // CommandPlayer도 네트워크로 생성
+        Vector3 pos = cmdRedSpawnPoint.position;
+        Quaternion rot = cmdRedSpawnPoint.rotation;
+        var go = PhotonNetwork.Instantiate("CommandPlayer", pos, rot);
+
+        // Canvas 세팅 (내꺼만)
+        if (go.GetComponent<PhotonView>().IsMine)
+        {
+            var commandPlayer = go.GetComponent<CommandPlayer>();
+            var canvasObj = Instantiate(canvasPrefab);
+            commandPlayer.goldText = canvasObj.transform.Find("ResourcePanel/GoldText").GetComponent<TMP_Text>();
+            commandPlayer.gearText = canvasObj.transform.Find("ResourcePanel/GearText").GetComponent<TMP_Text>();
+            commandPlayer.playerInputHandler = canvasObj.GetComponent<PlayerInputHandler>();
+        }
+    }
+    #endregion
 }
