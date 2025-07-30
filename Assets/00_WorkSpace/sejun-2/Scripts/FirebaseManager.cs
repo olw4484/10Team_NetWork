3ba2b8530393c7a35b82c2a31ebf983ccd240df2
sejun <sejun2.lee@gmail.com>
2025-07-30 09:55:16 +0900

    [fix] git 버그로 날아간 빌드 복구 및 수정

--- Assets/00_WorkSpace/sejun-2/Scripts/FirebaseManager.cs
+++ Assets/00_WorkSpace/sejun-2/Scripts/FirebaseManager.cs
@@ -1,20 +1,24 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Firebase.Extensions;
-using Firebase.Auth;
-using Firebase;
 
 public class FirebaseManager : MonoBehaviour
 {
     private static FirebaseManager instance;
     public static FirebaseManager Instance {  get { return instance; } }
 
-    private static FirebaseApp app;
-    public static FirebaseApp App { get { return app; } }
+    private static FirebaseApp app; // FirebaseApp 인스턴스
+    public static FirebaseApp App { get { return app; } }   // FirebaseApp 인스턴스에 접근하기 위한 프로퍼티
 
     private static FirebaseAuth auth;
     public static FirebaseAuth Auth { get { return auth; } }
+
+    private static FirebaseDatabase database;
+    public static FirebaseDatabase Database { get { return database; } }
 
     private void Awake()
     {
@@ -36,14 +40,16 @@ public class FirebaseManager : MonoBehaviour
             if (dependencyStatus == Firebase.DependencyStatus.Available)
             {
                 Debug.Log("파이어 베이스 설정이 모두 충족되어 사용할 수 있는 상황");
-                app = FirebaseApp.DefaultInstance;
-                auth = FirebaseAuth.DefaultInstance;
+                app = FirebaseApp.DefaultInstance;  // FirebaseApp 인스턴스 초기화
+                auth = FirebaseAuth.DefaultInstance;    // FirebaseAuth 인스턴스 초기화
+                database = FirebaseDatabase.DefaultInstance;    // FirebaseDatabase 인스턴스 초기화
             }
             else
             {
                 Debug.LogError($"파이어 베이스 설정이 충족되지 않아 실패했습니다. 이유: {dependencyStatus}");
                 app = null;
                 auth = null;
+                database = null;
             }
         });
 
