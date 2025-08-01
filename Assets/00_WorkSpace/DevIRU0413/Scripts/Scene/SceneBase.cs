a0dcc5d0cf072c97afba80c9292658fd4acb5099
DevIRU <deviru0413@gmail.com>
2025-08-01 12:27:26 +0900

    [Refactor]: 초기화 순서 구조 변경으로 인한 코드 수정

--- Assets/00_WorkSpace/DevIRU0413/Scripts/Scene/SceneBase.cs
+++ Assets/00_WorkSpace/DevIRU0413/Scripts/Scene/SceneBase.cs
@@ -5,7 +5,7 @@ public abstract class SceneBase : MonoBehaviour
     // 현재 씬의 고유 ID (상속 클래스에서 지정)
     public abstract SceneID SceneID { get; }
 
-    [SerializeField] private GameObject[] m_uiPrefabs;
+    [SerializeField] private GameObject[] managersInOrder;
 
     protected void Awake()
     {
@@ -19,12 +19,15 @@ public abstract class SceneBase : MonoBehaviour
     private void LoadUI()
     {
         // UI 프리팹을 순회하며 UI 매니저에 설정
+
+        // 현재 씬에 존재하는 BaseUI 오브젝트 수집
+        // GameObject[] SubscribeManagers = GameObject.FindGameObjectsWithTag("Manager");
     }
 
     // 매니저 등록 및 초기화
     private void LoadManagers()
     {
-        Debug.Log($"Scene {SceneID} Load Managers.");
+        Debug.Log($"[SceneBase]: Scene {SceneID} Load Managers.");
 
         // 현재 씬에 존재하는 Manager 태그 오브젝트 수집
         GameObject[] SubscribeManagers = GameObject.FindGameObjectsWithTag("Manager");
