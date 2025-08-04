7b991f94f45a1107ed230b4fba16402c415eb41a
NormalSeed <dlrjsghk11@naver.com>
2025-08-04 18:44:38 +0900

    [Feat] Hero HpBarUI
    Hero 플레이어 HP바 기능 구현
    네트워크 동기화 작업 필요

---
+++ Assets/00_WorkSpace/NormalSeed/Scripts/Hero/HeroHPBarUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroHPBarUI : MonoBehaviour
+{
+    private Canvas canvas;
+    private RectTransform rect;
+    public Vector3 offset;
+    public Image hpImage;
+    public Transform targetTransform;
+
+    private void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        rect = GetComponent<RectTransform>();
+        offset = new Vector3(0, 60, 0);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(targetTransform.position);
+        rect.position = screenPos + offset;
+    }
+}
