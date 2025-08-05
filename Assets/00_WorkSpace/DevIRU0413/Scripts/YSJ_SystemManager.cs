9eaee5dde115ba411310a3d1fa93936f562b0328
DevIRU <deviru0413@gmail.com>
2025-08-05 09:50:47 +0900

    [Feat]: SystemManager 기능 수정

--- Assets/00_WorkSpace/DevIRU0413/Scripts/YSJ_SystemManager.cs
+++ Assets/00_WorkSpace/DevIRU0413/Scripts/YSJ_SystemManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,7 +26,6 @@ public class YSJ_SystemManager : YSJ_SimpleSingleton<YSJ_SystemManager>, IManage
 
     public SystemStateType CurrentState { get; private set; }
 
-    public int Priority => (int)ManagerPriority.SystemManager;
     public bool IsDontDestroy => isDontDestroyOnLoad;
     #endregion
 
@@ -34,17 +34,7 @@ public class YSJ_SystemManager : YSJ_SimpleSingleton<YSJ_SystemManager>, IManage
     {
         if (_isInitialized) return;
 
-        GameObject sceneBaseGO = GameObject.FindGameObjectWithTag("SceneBase");
-        SceneBase sceneBaseCmp = sceneBaseGO.GetComponent<SceneBase>();
-
-        if (sceneBaseCmp == null)
-        {
-            Debug.LogError("SceneBase 컴포넌트가 존재하지 않습니다. 시스템 매니저 초기화 실패.");
-            return;
-        }
-
-        CurrentSceneBase = sceneBaseCmp;
-        CurrentSceneID = sceneBaseCmp.SceneID;
+        SceneLoaded();
         ChangeState(SystemStateType.Init);
 
         _isInitialized = false;
@@ -68,6 +58,10 @@ public class YSJ_SystemManager : YSJ_SimpleSingleton<YSJ_SystemManager>, IManage
     public void LoadSceneWithPreActions(string sceneName, List<(string message, Func<IEnumerator> action)> preActions = null, string finalMessage = "씬 로딩 중...")
     {
         StartCoroutine(TransitionScene(sceneName, preActions, finalMessage));
+    }
+    public void LoadPhotonSceneWithPreActions(string sceneName, List<(string message, Func<IEnumerator> action)> preActions = null, string finalMessage = "씬 로딩 중...") 
+    {
+        StartCoroutine(TransitionPhotonScene(sceneName, preActions, finalMessage));
     }
 
     private IEnumerator TransitionScene(string sceneName, List<(string message, Func<IEnumerator> action)> preActions = null, string finalMessage = "씬 로딩 중...")
@@ -98,7 +92,60 @@ public class YSJ_SystemManager : YSJ_SimpleSingleton<YSJ_SystemManager>, IManage
         }
 
         LoadingUI?.Hide(); // 나중에 따로 빼도 됨
-        ChangeState(SystemStateType.Playing);
+
+        SceneLoaded();
+    }
+    private IEnumerator TransitionPhotonScene(string sceneName, List<(string message, Func<IEnumerator> action)> preActions = null, string finalMessage = "씬 로딩 중...")
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            Debug.LogError("Photon에 연결되어 있지 않습니다.");
+            yield break;
+        }
+
+        ChangeState(SystemStateType.SceneChange);
+        LoadingUI?.Show("초기화 중...");
+
+        // 전처리 액션
+        if (preActions != null)
+        {
+            foreach (var (message, action) in preActions)
+            {
+                if (!string.IsNullOrEmpty(message))
+                    LoadingUI?.UpdateMessage(message);
+
+                if (action != null)
+                    yield return StartCoroutine(action());
+            }
+        }
+
+        LoadingUI?.UpdateMessage(finalMessage);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(sceneName); // 모든 플레이어에게 전파됨
+        }
+        else
+        {
+            // 비마스터는 수동으로 로딩 메시지만 보여줌
+            LoadingUI?.UpdateMessage("마스터 클라이언트가 씬을 전환 중입니다...");
+        }
+
+        LoadingUI?.Hide();
+    }
+
+    private void SceneLoaded()
+    {
+        GameObject sceneBaseGO = GameObject.FindGameObjectWithTag("SceneBase");
+        SceneBase sceneBaseCmp = sceneBaseGO?.GetComponent<SceneBase>();
+        if (sceneBaseCmp == null)
+        {
+            Debug.LogError("SceneBase 컴포넌트가 존재하지 않습니다.");
+            return;
+        }
+
+        CurrentSceneBase = sceneBaseCmp;
+        CurrentSceneID = sceneBaseCmp.SceneID;
     }
 
     #endregion
@@ -133,7 +180,6 @@ public class YSJ_SystemManager : YSJ_SimpleSingleton<YSJ_SystemManager>, IManage
     {
         return PlayerPrefs.GetFloat("Volume", 0.5f);
     }
-
 
 
     #endregion
