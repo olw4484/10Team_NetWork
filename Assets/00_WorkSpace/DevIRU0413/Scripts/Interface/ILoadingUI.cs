52f5fae486ccadbfd8b212da5f28f1e5d8980974
DevIRU <deviru0413@gmail.com>
2025-08-06 12:57:28 +0900

    [Feat]: 로딩 관련 코드 수정

--- Assets/00_WorkSpace/DevIRU0413/Scripts/Interface/ILoadingUI.cs
+++ Assets/00_WorkSpace/DevIRU0413/Scripts/Interface/ILoadingUI.cs
@@ -1,6 +1,10 @@
-public interface ILoadingUI
+﻿public interface ILoadingUI
 {
+    void Init();
     void Show(string message);
     void Hide();
     void UpdateMessage(string message);
+
+    float GetProgress();
+    void SetProgress(float value);
 }
