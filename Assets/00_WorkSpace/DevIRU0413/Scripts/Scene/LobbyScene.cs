179206e75ce3c6378b7715a8488a19150dd4db5f
DevIRU <deviru0413@gmail.com>
2025-08-08 14:14:23 +0900

    [Fix]: 씬 전환시 로딩 창 관련 버그 수정

---
+++ Assets/00_WorkSpace/DevIRU0413/Scripts/Scene/LobbyScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LobbyScene : SceneBase
+{
+    public override SceneID SceneID => SceneID.LobbyScene;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+}
