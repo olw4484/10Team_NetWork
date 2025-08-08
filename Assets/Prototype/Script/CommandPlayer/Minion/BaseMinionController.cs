81e50dd7e6a527f44f9a50af708e78dde8647605
owl4484 <rodeo23267286@gmail.com>
2025-08-08 15:32:18 +0900

    [Chore] 안쓰는 코드 삭제
    - 안쓰는 코드 삭제

--- Assets/Prototype/Script/CommandPlayer/Minion/BaseMinionController.cs
+++ Assets/Prototype/Script/CommandPlayer/Minion/BaseMinionController.cs
@@ -154,19 +154,6 @@ public abstract class BaseMinionController : MonoBehaviour, IDamageable, IPunIns
             isFollowingWaypoint = true;
             MoveToNextWaypoint();
         }
-
-        // --- RPC를 사용한 애니메이션 동기화 로직 ---
-        if (photonView.IsMine)
-        {
-            bool currentIsMovingState = animator.GetBool("IsMoving");
-            bool newIsMovingState = agent.velocity.magnitude > 0.1f && !agent.isStopped;
-
-            if (currentIsMovingState != newIsMovingState)
-            {
-                // 상태가 변경되었을 때만 모든 클라이언트에 RPC 호출
-                photonView.RPC(nameof(RPC_SetMoving), RpcTarget.All, newIsMovingState);
-            }
-        }
     }
 
     /// <summary>
@@ -493,13 +480,6 @@ public abstract class BaseMinionController : MonoBehaviour, IDamageable, IPunIns
     public void RPC_SyncRotation(float x, float y, float z, float w)
     {
         transform.rotation = new Quaternion(x, y, z, w);
-    }
-
-    [PunRPC]
-    private void RPC_SetMoving(bool isMoving)
-    {
-        //animator.SetBool("IsMoving", isMoving);
-        Debug.Log("Movement state received: " + isMoving);
     }
     #endregion
 }
